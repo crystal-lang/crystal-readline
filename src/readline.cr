823ac0b0fe9a52288014a9c738123f1e5941fee7
Johannes Müller <straightshoota@gmail.com>
2021-10-19 16:57:15 +0200

    Extract libreadline.cr

--- src/readline.cr
+++ src/readline.cr
@@ -1,22 +1,4 @@
-@[Link("readline")]
-{% if flag?(:openbsd) %}
-  @[Link("termcap")]
-{% end %}
-lib LibReadline
-  alias Int = LibC::Int
-
-  fun readline(prompt : UInt8*) : UInt8*
-  fun add_history(line : UInt8*)
-  fun rl_bind_key(key : Int, f : Int, Int -> Int) : Int
-  fun rl_unbind_key(key : Int) : Int
-
-  alias CPP = (UInt8*, Int, Int) -> UInt8**
-
-  $rl_attempted_completion_function : CPP
-  $rl_line_buffer : UInt8*
-  $rl_point : Int
-  $rl_done : Int
-end
+require "./libreadline"
 
 private def malloc_match(match)
   match_ptr = LibC.malloc(match.bytesize + 1).as(UInt8*)
